37e14a0541ed82227d421a71b6f0dcd86f7f9338
Carlo Evidente <156107894+caevi@users.noreply.github.com>
2024-12-12 11:45:36 -0500

    media queries and add ons

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import './Videos.css'; // Import styles for the videos component
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import Socials from './Socials';
+
 const Videos = () => {
   return (
     <div className="videos">
-      <h1></h1>
-      <div class="video-container">
-  <iframe 
-    width="600" 
-    height="400" 
-    src="https://www.youtube.com/embed/mIAdpmObvlU?si=vuVOXley10aFY5Km" 
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    referrerpolicy="strict-origin-when-cross-origin" 
-    allowfullscreen>
-  </iframe>
-</div>
+      <div className="video-container">
+        <iframe 
+          width="600" 
+          height="400" 
+          src="https://www.youtube.com/embed/mIAdpmObvlU?si=vuVOXley10aFY5Km" 
+          title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerPolicy="strict-origin-when-cross-origin" 
+          allowFullScreen
+        ></iframe>
       </div>
 
+      {/* "Check Out My Mixes" Button */}
+      <div className="check-out-mixes-container">
+        <Link to="/mixes">
+          <button className="check-out-mixes-button">
+            Check Out My Mixes
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
