4b79ec426679b6c55185ab6a2f90e12155bcf917
caevi <156107894+caevi@users.noreply.github.com>
2024-12-12 12:34:56 -0500

    update video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,16 +7,7 @@ const Videos = () => {
   return (
     <div className="videos">
       <div className="video-container">
-        <iframe 
-          width="600" 
-          height="400" 
-          src="https://www.youtube.com/embed/mIAdpmObvlU?si=vuVOXley10aFY5Km" 
-          title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          referrerPolicy="strict-origin-when-cross-origin" 
-          allowFullScreen
-        ></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Vsr70INp-GM?si=usMCcwd6iQ61jNF9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
 
       {/* "Check Out My Mixes" Button */}
